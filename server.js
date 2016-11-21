f8b9b7828e74e3374726e2a6cdecd5c9f5d5231b
MADHU-BAJPAI <madhubajpai111@gmail.com>
2016-11-21 17:46:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+app.get('/ui/web.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'web.html'));
 });
 
