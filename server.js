cab29f8d789276553d1515d2423abea90b91cda7
MADHU-BAJPAI <madhubajpai111@gmail.com>
2016-11-22 21:07:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'web.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/newcss.css', function (req, res) {
